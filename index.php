6dfa75ab5f754caedd57851f136811b9a39572d0
Dennis Cardoso <dennis.cardoso@outlook.com>
2016-02-15 01:39:08 -0200

    4rodas page

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <! DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<header>
+			<div>
+				<h1>Quatro Rodas</h1>
+				<nav class "main-menu">
+					<ul>
+						<li><a href="#">Carros</a></li>
+						<li><a href="#">Testes</a></li>
+						<li><a href="#">Notícias</a></li>
+						<li><a href="#">Auto-Serviço</a></li>
+						<li><a href="#">Guia de Compras</a></li>
+						<li><a href="#">Tabela Fipe</a></li>
+						<li><a href="#">Assine</a></li>
+					</ul>
+
+			</div>
+		</header>
+	</body>
 </html>
