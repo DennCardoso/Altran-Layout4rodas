f25691d6c4ce62b6c37a341a15d547085450f4f9
Dennis Cardoso <dennis.cardoso@outlook.com>
2016-02-15 09:24:43 -0200

    Meta

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <! DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title></title>
+		<meta charset="UTF-8">
+		<title>Quatro Roda - HomePage</title>
 	</head>
 	<body>
 		<header>
 			<div>
 				<h1>Quatro Rodas</h1>
-				<nav class "main-menu">
+				<nav class="main-menu">
 					<ul>
 						<li><a href="#">Carros</a></li>
 						<li><a href="#">Testes</a></li>
@@ -17,8 +18,25 @@
 						<li><a href="#">Tabela Fipe</a></li>
 						<li><a href="#">Assine</a></li>
 					</ul>
-
 			</div>
-		</header>
+			<!-- 						Submenu Mais acessados					 		-->
+			<div>
+				<h2>.Acessados</h2>
+				<nav class="sub_menu">
+					<ul>
+						<li><a href="#">Salão do Automóvel</a></li>
+						<li><a href="#">Renegade</a></li>
+						<li><a href="#">Novo Sandero</a></li>
+						<li><a href="#">Novo Fox</a></li>
+						<li><a href="#">Novo Ka</a></li>
+						<li><a href="#">HB 20</a></li> 
+						<li><a href="#">Duster</a></li>
+						<li><a href="#">Golf</a></li>
+						<li><a href="#">Corola</a></li>
+						<li><a href="#">Civic</a></li>
+						<li><a href="#">|A-Z|</a></li>
+					</ul>
+				</nav>
+			</header>	
 	</body>
 </html>
